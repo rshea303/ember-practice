4463d06d5116045ae280c832941999397bcfe62c
rshea303 <rshea@wombatsecurity.com>
2016-02-11 09:22:24 -0700

    add validations

--- app/notebooks/notes/route.js
+++ app/notebooks/notes/route.js
@@ -1,28 +1,36 @@
 import Ember from 'ember';
+import ValidationFunctions from 'ember-note/mixins/validation-functions';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ValidationFunctions, {
   model: function(params) {
     return this.store.query('note', {notebook:params.notebook_id});
   },
   actions: {
     addNote: function() {
-      this.store.findRecord('notebook', 
-        this.paramsFor('notebooks.notes').notebook_id).then(
-          (notebook) => {
-            console.log(notebook);
-            var note = this.store.createRecord('note', {
-              title : this.controller.get('title'),
-              notebook: notebook
+      var title = this.controller.get('title');
+      if(!this.isValidLength(title, 0, 140)) {
+        alert('Title must be longer than 0 '
+          + 'characters and not more than 140.');
+      }
+      else {
+        this.store.findRecord('notebook', 
+          this.paramsFor('notebooks.notes').notebook_id).then(
+            (notebook) => {
+              console.log(notebook);
+              var note = this.store.createRecord('note', {
+                title : this.controller.get('title'),
+                notebook: notebook
+              });
+              console.log(note);
+              note.save().then(() => {
+                console.log('save successful');
+                this.controller.set('title',null);
+                this.refresh();
+              }, function() {
+                console.log('save failed');
+              });
             });
-            console.log(note);
-            note.save().then(() => {
-              console.log('save successful');
-              this.controller.set('title',null);
-              this.refresh();
-            }, function() {
-              console.log('save failed');
-            });
-          });
+      }
     },
     deleteNote: function(note) {
       console.log('deleting note with title ' + note.get('title'));
